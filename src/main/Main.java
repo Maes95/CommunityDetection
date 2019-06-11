3c0176ec6fc1661b260435bd5ce4ed3ca16e5a52
Maes95 <michel.maes95@gmail.com>
2019-06-11 17:38:53 +0200

    backup 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,7 @@ import grafo.optilib.tools.RandomManager;
 import structure.DividerInstance;
 import structure.DividerSolution;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
@@ -18,18 +19,58 @@ public class Main {
 
     private static final int ITERS = 100;
     private static final String PATH_TO_GRAPHS_TRAIN = "graphs/train/";
+    private static final String PATH_TO_GRAPHS_TEST = "graphs/test/";
     private static final double[] ALPHAS = {0.25, 0.5, 0.75};
     private static final int N_ALGORITHMS = 8+1;
     private static final AlgorithmExecutor.EXECUTOR_TYPE EXECUTOR_TYPE = AlgorithmExecutor.EXECUTOR_TYPE.MULTI;
 
     public static void main(String[] args) throws Exception {
         RandomManager.setSeed(123);
-        train();
+        test();
+        //train();
+    }
+
+    public static void test() throws Exception {
+        FileWriter csvWriter = new FileWriter("results/results-test.csv");
+        csvWriter.append("Instance, N, M, DividerGreedyLS 0.25,,,,\n");
+        csvWriter.append(",,,Mod,T(ms), Dev(%), # Best\n");
+
+        File[] files = new File(PATH_TO_GRAPHS_TEST).listFiles();
+        for (File file : files) {
+            if (file.isFile() && file.getName().endsWith(".txt")) {
+
+                DividerInstance instance = new DividerInstance(PATH_TO_GRAPHS_TEST+file.getName());
+
+                System.out.println("--- GREEDY LOCAL SEARCH ---");
+                double alpha = 0.25;
+                AlgorithmExecutor aex = new AlgorithmExecutor(EXECUTOR_TYPE);
+                DividerSolution greedyLSSolution = aex.calculateBestSolution(new ConstDividerGreedyLS(alpha), instance, 100);
+                System.out.println("ALPHA: "+alpha+" MOD: "+greedyLSSolution.getModularity());
+
+                String[] row = {
+                        file.getName(), // Instance
+                        Integer.toString(instance.getN()), // N
+                        Integer.toString(instance.getM()) // M
+                };
+
+                String[] aux = getRow(greedyLSSolution,0,false);
+                row = concat(row, aux);
+                csvWriter.append(String.join(",", row)+"\n");
+                csvWriter.flush();
+
+                String solution = greedyLSSolution.getSolutionAsList().toString();
+                String filename = "results/community_"+file.getName();
+                BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+                bw.write(solution);
+                bw.close();
+            }
+        }
+
     }
 
     public static void train() throws Exception {
 
-        FileWriter csvWriter = new FileWriter("results-train.csv");
+        FileWriter csvWriter = new FileWriter("results/results-train.csv");
         String header1 = "Instance, N, M, " +
                 "Random,,,,"+
                 "Divider,,,,"+
